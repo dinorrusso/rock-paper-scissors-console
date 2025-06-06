fdecbec67e12bc5c8aa3b77e0d1f557d01e5db7f
dinorrusso <drr.developer.1972@gmail.com>
2025-06-06 13:49:45 -0400

    Exercise Complete

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Simple javascript project to create rock paper scissors came in console
+Simple javascript project to create rock paper scissors game with a UI using javascript and CSS.
+
+This is what the result looks like.
+![alt text](127.0.0.1_5500_index.html.png)
--- script.js
+++ script.js
@@ -1,74 +1,112 @@
+let roundCount = 0;
+let humanScore = 0;
+let computerScore = 0;
+let draws = 0;
+
+//functions to support computer play
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 function getComputerChoice() {
-    switch (getRandomInt(3)) {
-        case 0:
-            return "rock";
-            break;
-        case 1:
-            return "paper";
-            break;
-        case 2:
-            return "scissors";
-            break;
-    }
+  switch (getRandomInt(3)) {
+    case 0:
+      return "Rock";
+      break;
+    case 1:
+      return "Paper";
+      break;
+    case 2:
+      return "Scissors";
+      break;
+  }
 }
-function getHumanChoice() {
-    return prompt(`Game ${gameNumber} of ${numGames}: Enter either: rock, paper, or scissors.`)
+// add round results to the scoreboard
+function addRowToScoreboard(round, pChoice, cChoice, resultText) {
+  const table = document.getElementById("scoreboard");
+  const tableBody = table.querySelector("tbody");
+
+  const newRow = tableBody.insertRow(-1);
+
+  const roundCell = newRow.insertCell(-1);
+  roundCell.textContent = round;
+
+  const playerCell = newRow.insertCell(-1);
+  playerCell.textContent = pChoice;
+
+  const computerCell = newRow.insertCell(-1);
+  computerCell.textContent = cChoice;
+
+  const resultCell = newRow.insertCell(-1);
+  resultCell.textContent = resultText;
 }
 
+//game logic for playing and scoring one round
+//called by the button event listener
 function playRound(computerChoice, humanChoice) {
-    console.log(`your choice:${humanChoice}`)
-    console.log(`computer choice:${computerChoice}`)
-    if (computerChoice === humanChoice) {
-        console.log("It's a draw.")
-        draws++;
-    } else if (computerChoice === "rock" && humanChoice === "scissors") {
-        console.log("Computer Wins - rock breaks scissors")
-        computerScore++;
-    } else if (computerChoice === "paper" && humanChoice === "rock") {
-        console.log("Computer Wins - paper covers rock")
-        computerScore++;
-    } else if (computerChoice === "scissors" && humanChoice === "paper") {
-        console.log("Computer Wins - scissors cuts paper")
-        computerScore++;
-    } else if (computerChoice === "rock" && humanChoice === "paper") {
-        console.log("You Win - paper covers rock")
-        humanScore++;
-    } else if (computerChoice === "paper" && humanChoice === "scissors") {
-        console.log("You Win - scissors cut paper")
-        humanScore++;
-    } else if (computerChoice === "scissors" && humanChoice === "rock") {
-        console.log("You Win - rock breaks scissors")
-        humanScore++;
-    }
-    console.log(`You ${humanScore}, Computer ${computerScore} Draws ${draws}`);
-}
-function playGame(numberOfRounds) {
-    for (let i = 0; i < numberOfRounds; i++) {
-        let hc = "";
-        let isInvalid = true;
+  var resultText;
+  roundCount++;
 
-        while (isInvalid) {
-            hc = getHumanChoice();
-            if (typeof  hc === "string"){
-                hc = hc.toLowerCase();
-                if (hc === 'rock' || hc === 'paper' || hc === 'scissors') {
-                    isInvalid = false;
-                }
-            }
-        }
-        playRound(getComputerChoice(), hc);
-        gameNumber++;
-    }
+  //score it
+  if (computerChoice === humanChoice) {
+    resultText = "It's a draw.";
+    draws++;
+  } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+    resultText = "Computer Wins - rock breaks scissors";
+    computerScore++;
+  } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+    resultText = "Computer Wins - paper covers rock";
+    computerScore++;
+  } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+    resultText = "Computer Wins - scissors cuts paper";
+    computerScore++;
+  } else if (computerChoice === "Rock" && humanChoice === "Paper") {
+    resultText = "Player Wins - paper covers rock";
+    humanScore++;
+  } else if (computerChoice === "Paper" && humanChoice === "Scissors") {
+    resultText = "Player Wins - scissors cut paper";
+    humanScore++;
+  } else if (computerChoice === "Scissors" && humanChoice === "Rock") {
+    resultText = "Player Wins - rock breaks scissors";
+    humanScore++;
+  }
+  addRowToScoreboard(roundCount, humanChoice, computerChoice, resultText);
+  //update the stats under teh scoreboard
+  stats = document.querySelector("p#stats");
+  stats.textContent = `Player Score: ${humanScore} Computer Score: ${computerScore} Draws: ${draws}`;
 }
 
-console.log("This is Rock Paper Scissors (console edition)");
-let computerScore = 0;
-let humanScore = 0;
-let draws = 0;
-let gameNumber = 1;
-const numGames = 5;
-playGame(numGames);
-console.log(`Final score: You ${humanScore}, Computer ${computerScore} Draws ${draws}`);
+//get the ui container
+uiArea = document.querySelector("div.ui-container");
+// uiArea.style.backgroundColor = 'yellow';
+uiArea.style.fontSize = "24px";
+
+//get the button area set up
+btnArea = document.querySelector("div.btn-container");
+
+rockBtn = document.createElement("button");
+rockBtn.textContent = "Rock";
+btnArea.appendChild(rockBtn);
+
+paperBtn = document.createElement("button");
+paperBtn.textContent = "Paper";
+btnArea.appendChild(paperBtn);
+
+scissorsBtn = document.createElement("button");
+scissorsBtn.textContent = "Scissors";
+btnArea.appendChild(scissorsBtn);
+
+//add event listener and common styles
+buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  //set common styles
+  button.style.fontSize = "24px";
+  button.style.padding = "8px 16px";
+  button.style.marginLeft = "16px";
+  button.style.marginRight = "8px";
+  button.style.width = "125px";
+  button.style.backgroundColor = "#1b5abf";
+  button.style.color = "white";
+  button.addEventListener("click", () => {
+    playRound(getComputerChoice(), button.textContent);
+  });
+});
